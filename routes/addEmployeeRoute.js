cee9014f5e1d242f748df3f87c7df36604237c84
Ankit Fukte <100926362+Jarvis9960@users.noreply.github.com>
2023-03-10 11:58:22 +0530

    deleting directory

--- routes/addEmployeeRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { addEmployeeController, getEmployeeController } = require("../controllers/employeeController");
-const { protectedRoute } = require("../middlewares/protectedMiddleware");
-const router = express.Router();
-
-router.post("/addemployee", protectedRoute, addEmployeeController);
-router.get("/getallemployees", protectedRoute, getEmployeeController);
-
-module.exports = router;
--- routes/loginRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { loginController } = require("../controllers/authController");
-
-router.post("/login", loginController);
-
-module.exports = router;
