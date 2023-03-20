9afac62f1cb28b1974b3e0dec2df0e71f43286d3
Ankit Fukte <100926362+Jarvis9960@users.noreply.github.com>
2023-03-21 00:04:28 +0530

    adding allemployeeworkinghours

--- controllers/workingHourController.js
+++ controllers/workingHourController.js
@@ -35,4 +35,28 @@ const getEmployeeWorkingHourByEmail = async (req, res) => {
   }
 };
 
-module.exports = { getEmployeeWorkingHourByEmail }
+const getAllEmployeeWorkingHour = async (req, res) => {
+  try {
+    const allEmployeeWorkingHour = await employeeWorkingHour
+      .find()
+      .populate("Employee");
+
+    if (!allEmployeeWorkingHour) {
+      return res
+        .status(422)
+        .json({ status: false, message: "employee collection is empty" });
+    }
+
+    return res
+      .status(201)
+      .json({
+        status: true,
+        message: "working hours succesfully fetched",
+        allEmployeeWorkingHour,
+      });
+  } catch (error) {
+    return res.status(422).json({status: false, message: "something went wrong"})
+  }
+};
+
+module.exports = { getEmployeeWorkingHourByEmail, getAllEmployeeWorkingHour }
