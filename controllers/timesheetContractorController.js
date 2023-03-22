25e9ca485749b00f2c7c215f9a9a64bf45e1078d
Ankit Fukte <100926362+Jarvis9960@users.noreply.github.com>
2023-03-22 10:59:34 +0530

    adding timesheet controller

---
+++ controllers/timesheetContractorController.js
@@ -0,0 +1,49 @@
+const TimesheetContractor = require("../models/timesheetContractorSchema");
+
+const addTimesheet = async (req, res) => {
+  try {
+    const { employeeName, timeSheet } = req.body;
+
+    const employeeNameExist = TimesheetContractor.findOne({
+      EmployeeName: employeeName,
+    });
+
+    if (!employeeNameExist) {
+      const newEmployeeTimesheet = new TimesheetContractor({
+        EmployeeName: employeeName,
+        Timesheet: [timeSheet],
+      });
+
+      const savedNewEmployeeTimesheet = await newEmployeeTimesheet.save();
+
+      console.log(savedNewEmployeeTimesheet);
+
+      if (savedNewEmployeeTimesheet) {
+        return res.status(201).json({
+          status: true,
+          message: "successfully created newTimesheet",
+          savedNewEmployeeTimesheet,
+        });
+      }
+    } else {
+      const updateEmployeeTimesheet = await TimesheetContractor.updateOne(
+        { EmployeeName: employeeName },
+        { $push: { Timesheet: timeSheet } }
+      );
+
+      console.log(updateEmployeeTimesheet);
+
+      if (updateEmployeeTimesheet) {
+        return res
+          .status(201)
+          .json({ status: true, message: "successfully created timesheet" });
+      }
+    }
+  } catch (error) {
+    return res
+      .status(422)
+      .json({ status: false, message: "something went wrong", err: error });
+  }
+};
+
+module.exports = { addTimesheet };
