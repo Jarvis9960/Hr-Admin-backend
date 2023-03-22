86a36068cfce5eb7a3403004b34998b1439477e3
Ankit Fukte <100926362+Jarvis9960@users.noreply.github.com>
2023-03-22 13:53:01 +0530

    adding sortedTimesheet route

--- routes/timesheetContractorRoute.js
+++ routes/timesheetContractorRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { addTimesheet, getTimesheet } = require("../controllers/timesheetContractorController");
+const { addTimesheet, getTimesheet, getSortedData } = require("../controllers/timesheetContractorController");
 
 router.post("/addtimesheet", addTimesheet);
 router.get("/gettimesheet", getTimesheet);
+router.get("/getsortedtimesheet", getSortedData)
 
 
 module.exports = router;
