6adbdd3fb658492f59797cdf24385e8b8ba00ffd
Ankit Fukte <100926362+Jarvis9960@users.noreply.github.com>
2023-03-24 11:18:14 +0530

    adding middleware protection to contractor

--- middlewares/protectedMiddleware.js
+++ middlewares/protectedMiddleware.js
@@ -56,3 +56,31 @@ module.exports.protectedRouteForEmployee = async (req, res, next) => {
     return res.status(422).json({ message: "No token" });
   }
 };
+
+module.exports.protectedRouteForContractor = async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      if (!token) {
+        throw new Error("something went wrong");
+      }
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await Contractor.findOne({ _id: decoded.contractor_id });
+
+      next();
+    } catch (error) {
+      return res.status(442).json({ message: "Invalid Auth" });
+    }
+  } else {
+    return res.status(422).json({ message: "No token" });
+  }
+};
+
+
