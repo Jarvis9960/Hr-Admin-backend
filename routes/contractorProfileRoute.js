859e04981cfc73f9c859b8f286074e49d2f09d73
Ankit Fukte <100926362+Jarvis9960@users.noreply.github.com>
2023-03-24 11:04:26 +0530

    adding contractor profile route

---
+++ routes/contractorProfileRoute.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const { contractorProfileController } = require("../controllers/contractorProfileController");
+const router = express.Router();
+// const multer = require("multer");
+// const path = require("path");
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, path.join(__dirname, "../public/uploads"));
+//   },
+//   filename: function (req, file, cb) {
+//     cb(
+//       null,
+//       file.originalname + "-" + Date.now() + path.extname(file.originalname)
+//     );
+//   },
+// });
+
+// function checkFileType(file, cb) {
+//   const filetypes = /jpg|jpeg|png/;
+//   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+//   const mimetype = filetypes.test(file.mimetype);
+
+//   if (extname && mimetype) {
+//     return cb(null, true);
+//   } else {
+//     cb("Images only!"); // custom this message to fit your needs
+//   }
+// }
+
+// const upload = multer({
+//   storage,
+//   fileFilter: function (req, file, cb) {
+//     checkFileType(file, cb);
+//   },
+// });
+
+// const multipleUpload = upload.fields([{ name: "Panimage", maxCount: 1 }, {name: "Adharimage", maxCount: 1}]);
+
+const {
+  protectedRouteForEmployee,
+  protectedRoute,
+} = require("../middlewares/protectedMiddleware");
+
+router.post("/addcontractorprofile", contractorProfileController)
+
+
+
+module.exports = router;
