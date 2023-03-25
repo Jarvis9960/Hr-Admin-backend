7ac6a46df49d215be59e007086b9cf47a78c84a6
Ankit Fukte <100926362+Jarvis9960@users.noreply.github.com>
2023-03-25 14:43:47 +0530

    adding routes

--- routes/timesheetContractorRoute.js
+++ routes/timesheetContractorRoute.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { addTimesheet, getTimesheet, getSortedData, editTimesheetDateForAdmin, getTimesheetForContractor } = require("../controllers/timesheetContractorController");
+const { addTimesheet, getTimesheet, getSortedData, editTimesheetDateForAdmin, getTimesheetForContractor, editTimesheetDateForContractor } = require("../controllers/timesheetContractorController");
 
 router.post("/addtimesheet", addTimesheet);
 router.get("/gettimesheet", getTimesheet);
 router.get("/getsortedtimesheet", getSortedData);
 router.patch("/edittimesheetdateforadmin", editTimesheetDateForAdmin);
-router.get("/gettimesheetforcontractor", getTimesheetForContractor)
+router.get("/gettimesheetforcontractor", getTimesheetForContractor);
+router.patch("/edittimesheetdateforcontractor", editTimesheetDateForContractor);
 
 
 module.exports = router;
