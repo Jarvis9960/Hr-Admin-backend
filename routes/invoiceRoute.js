45707e30979c6ac950b7c8ce8184e560382f23ea
Ankit Fukte <100926362+Jarvis9960@users.noreply.github.com>
2023-03-29 14:39:56 +0530

    adding invoices route

--- routes/invoiceRoute.js
+++ routes/invoiceRoute.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   addInvoiceForEmployee,
   getInvoiceForEmployee,
+  getInvoiceOfContractor,
 } = require("../controllers/InvoiceController");
 
 router.post("/addinvoiceforemployee", addInvoiceForEmployee);
 router.get("/getinvoiceofemployee", getInvoiceForEmployee);
+router.get("/getinvoiceofcontractor", getInvoiceOfContractor);
 
 module.exports = router;
