fbb9536be36122125b23f884a9ec47a91db3f83b
Ankit Fukte <100926362+Jarvis9960@users.noreply.github.com>
2023-03-29 13:57:34 +0530

    adding contractor routes

--- routes/addEmployeeRoute.js
+++ routes/addEmployeeRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { addEmployeeController, getEmployeeController, getEmployeeforContractor } = require("../controllers/employeeController");
+const { addEmployeeController, getEmployeeController, getEmployeeforContractor, getAllContractorProfile} = require("../controllers/employeeController");
 const { protectedRoute } = require("../middlewares/protectedMiddleware");
 const router = express.Router();
 
 router.post("/addemployee", protectedRoute, addEmployeeController);
 router.get("/getallemployees", protectedRoute, getEmployeeController);
 router.get("/getallemployeesforcontrator", getEmployeeforContractor);
+router.get("/getcontractors", protectedRoute, getAllContractorProfile);
 
 module.exports = router;
