80b783b0de15fcbea624198d452c412035f1de65
Ankit Fukte <100926362+Jarvis9960@users.noreply.github.com>
2023-04-08 11:16:16 +0530

    updating route to take image

--- routes/invoiceRoute.js
+++ routes/invoiceRoute.js
@@ -7,8 +7,45 @@ const {
   approveInvoiceByAdmin,
 } = require("../controllers/InvoiceController");
 const { protectedRoute } = require("../middlewares/protectedMiddleware");
+const multer = require("multer");
+const path = require("path");
 
-router.post("/addinvoiceforemployee", addInvoiceForEmployee);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../public/uploads/invoicesign"));
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.originalname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb("Images only!"); // custom this message to fit your needs
+  }
+}
+
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+router.post(
+  "/addinvoiceforemployee",
+  upload.single("Sign"),
+  addInvoiceForEmployee
+);
 router.get("/getinvoiceofemployee", getInvoiceForEmployee);
 router.get("/getinvoiceofcontractor", getInvoiceOfContractor);
 router.patch("/approveinvoicebyadmin", protectedRoute, approveInvoiceByAdmin)
